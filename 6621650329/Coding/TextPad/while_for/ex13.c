51d68ec69fbd296f85844725977bbb0c91ff4159
BLol-C <bozzisx@gmail.com>
2025-02-12 16:25:20 +0700

    Add files via upload

---
+++ 6621650329/Coding/TextPad/while_for/ex13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main(){
+
+	int a,b,c,d,e;
+	int cnt = 0;
+	int temp;
+
+	auto int x;
+
+
+	printf("Please input character 1: ");
+	scanf(" %c",&a);
+	printf("Please input character 2: ");
+	scanf(" %c",&b);
+	printf("Please input character 3: ");
+	scanf(" %c",&c);
+	printf("Please input character 4: ");
+	scanf(" %c",&d);
+	printf("Please input character 5: ");
+	scanf(" %c",&e);
+
+
+	while(cnt < 5) {
+		while(
+	}
+
+}
