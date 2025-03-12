0e83ac1ffde425a2cc8134317112d0dff191ec1b
BLol-C <bozzisx@gmail.com>
2025-03-12 16:13:25 +0700

    Delete C-lessons-main/6621650329/Coding directory

--- C-lessons-main/6621650329/Coding/TextPad/labsheet1_2/excerise p.1/Lab01_P04.c
+++
@@ -1,9 +0,0 @@
-  #include <stdio.h>
-  #include <conio.h>
-  main()
-  {
-	printf("\"Kasetsart University\"\n");
-	printf("\t\'Kasetsart University\'\n");
-	printf("\t\t** Faculty of Liberal Arts and Sciences **\n");
-
- }
